2bf7469432f689786361242f7cc2f92df7778a60
Jatin Pant <pantjatin239@gmail.com>
2026-09-04 16:55:05 +0530

    refactor: restructure project folders, clean up legacy configs and move auth files

--- src/config/initDb.js
+++
@@ -1,6 +0,0 @@
-async function initializeDatabase() {
-  console.log('Using SQLite database: database.sqlite (Auto-created)');
-  return Promise.resolve();
-}
-
-module.exports = initializeDatabase;
