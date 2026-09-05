ea654ae6f20da91c809c2963961b6e6ccba6d76d
aryanmishrahps-beep <aryanmishrahps@gmail.com>
2026-09-05 21:08:19 +0530

    Add getAllUsers endpoint and listUsers utility script

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -129,3 +129,12 @@ exports.getMe = async (req, res) => {
     res.status(500).json({ success: false, error: error.message });
   }
 };
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({}, '-password');
+    res.json({ success: true, count: users.length, users });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
